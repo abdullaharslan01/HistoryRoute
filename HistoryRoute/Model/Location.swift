98c17ba337e800c2cd089e12b02a87cac8fce584
abdullaharslan01 <abdullaharslan1473@gmail.com>
2024-06-16 16:31:20 +0300

    File structure created

---
+++ HistoryRoute/Model/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  HistoryRoute
+//
+//  Created by abdullah on 16.06.2024.
+//
+
+import MapKit
+
+struct Location{
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
