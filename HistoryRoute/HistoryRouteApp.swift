292dbc7e409d7ad5adb0524c806c249463c172ac
abdullaharslan01 <abdullaharslan1473@gmail.com>
2024-06-16 16:15:43 +0300

    Initial Commit

---
+++ HistoryRoute/HistoryRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryRouteApp.swift
+//  HistoryRoute
+//
+//  Created by abdullah on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HistoryRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
